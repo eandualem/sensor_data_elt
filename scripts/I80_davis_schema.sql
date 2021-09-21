109119d9bee97599a0fa8678568630317a2d732d
eandualem <eandualem@gmail.com>
2021-09-21 14:10:58 +0300

    feat: add sql schema for I80_davis

---
+++ scripts/I80_davis_schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS `TweetInformation` 
+(
+    `date` timestamp NOT NULL,
+    `time` timestamp NOT NULL,
+    `station_id` INT NOT NULL,
+    `col0` INT DEFAULT NULL,
+    `col1` INT DEFAULT NULL,
+    `col2` INT DEFAULT NULL,
+    `col3` INT DEFAULT NULL,
+    `col4` INT DEFAULT NULL,
+    `col5` INT DEFAULT NULL,
+    `col6` INT DEFAULT NULL,
+    `col7` INT DEFAULT NULL,
+    `col8` INT DEFAULT NULL,
+    `col9` INT DEFAULT NULL,
+    `col10` INT DEFAULT NULL,
+    `col11` INT DEFAULT NULL,
+    `col12` INT DEFAULT NULL,
+    `col13` INT DEFAULT NULL,
+    `col14` INT DEFAULT NULL,
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
