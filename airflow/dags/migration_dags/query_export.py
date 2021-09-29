30408bd3bb6c5fb0fa32728525ecdc390ebfc041
eandualem <eandualem@gmail.com>
2021-09-29 07:28:53 +0000

    feat: add redash query exporter script

---
+++ airflow/dags/migration_dags/query_export.py
@@ -0,0 +1,33 @@
+import click
+import requests
+from redash_toolbelt.client import Redash
+
+template = u"""/*
+Name: {name}
+Data source: {data_source}
+Created By: {created_by}
+Last Updated At: {last_updated_at}
+*/
+{query}"""
+
+
+def save_queries(queries):
+    for query in queries:
+        filename = "query_{}.sql".format(query["id"])
+        with open(filename, "w") as f:
+            content = template.format(
+                name=query["name"],
+                data_source=query["data_source_id"],
+                created_by=query["user"]["name"],
+                last_updated_at=query["updated_at"],
+                query=query["query"],
+            )
+            f.write(content)
+
+def export_queries(**kwargs):
+    redash_url = kwargs['redash_url']
+    api_key = kwargs['api_key']
+
+    redash = Redash(redash_url, api_key)
+    queries = redash.paginate(redash.queries)
+    save_queries(queries)
