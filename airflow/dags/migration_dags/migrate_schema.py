2ee29d4a3c7ac417190fa353cfb64f21d6898aa1
eandualem <eandualem@gmail.com>
2021-09-30 13:52:29 +0000

    feat: add script to migrate schema

---
+++ airflow/dags/migration_dags/migrate_schema.py
@@ -0,0 +1,30 @@
+import os
+import glob
+
+
+def convert_sql(sql_satement, target_path, file_name):
+
+  postgres_sql = sql_satement.lower()
+  postgres_sql = postgres_sql.replace(";", "")
+  postgres_sql = postgres_sql.strip()
+
+  postgres_sql = postgres_sql.replace("ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci".lower(), "")
+  postgres_sql = postgres_sql.replace("ENGINE=InnoDB DEFAULT CHARSET=utf8".lower(), "")
+  postgres_sql = postgres_sql.replace("double".lower(), "double precision")
+
+  postgres_sql = postgres_sql.replace("`", "")
+  postgres_sql = postgres_sql.strip() + ";"
+
+  if postgres_sql.split()[0] == 'load':
+    # to do load handler
+    pass
+  if postgres_sql.split()[0] == 'create':
+     with open(f'{target_path}/{file_name}', 'w', encoding = 'utf-8') as _file:
+      _file.write(postgres_sql)
+
+def convert_create_statements(src_path="/airflow/dags/mysqlSql/", tgt_path="/airflow/dags/postgresSql/"):
+  sql_files = glob.glob(f'{src_path}*.sql')
+  for sql_file in sql_files:
+    file_name = sql_file.split("/")[-1]
+    with open(sql_file, encoding = 'utf-8') as _file:
+      convert_sql(_file.read(), tgt_path, file_name=file_name)
